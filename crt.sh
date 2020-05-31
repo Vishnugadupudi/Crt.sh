95a409045059a54eac62b082c79ec5e8792c5adf
vishnu_vardhan_gadupudi <vishnuvardhangadupudi@gmail.com>
2020-05-31 22:42:54 +0530

    Modified Output Location

--- crt.sh
+++ crt.sh
@@ -16,10 +16,9 @@ echo "
 
 echo -e "Target ==> $1\n"
 
+curl -s https://crt.sh/\?q\=%.$1\&output\=json | jq -r '.[].name_value' | sed 's/\*\.//g' | sort -u | tee $1.txt ; 
 
-curl -s https://crt.sh/\?q\=%.$1\&output\=json | jq -r '.[].name_value' | sed 's/\*\.//g' | sort -u | tee hosts ; 
+echo -e "\nA total of `cat $1.txt | wc -l` subdomains were found for $1\n"
 
-
-echo -e "\nA total of `cat hosts | wc -l` subdomains were found for $1\n"
-
+echo -e "\n>> 📁 Subdomains for $1 were saved in: $1.txt \n"
 
